2d034e74d7da78de9e60a58e00fa36563473da28
Filip Stawicki <filip.stawicki74@gmail.com>
2018-11-12 17:47:16 +0100

    Add exchange_rates app and base model for storing exchange rates

---
+++ exchange_rates/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from djmoney.models.fields import CurrencyField
+
+
+class ExchangeRate(models.Model):
+
+    class Meta:
+        verbose_name = _('Exchange Rate')
+        verbose_name_plural = _('Exchange Rates')
+
+    source_currency = CurrencyField()
+    target_currency = CurrencyField()
+    rate = models.DecimalField(max_digits=12, decimal_places=6)
+    date = models.DateField()
